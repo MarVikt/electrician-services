043991a5e91c5c934c2f3c2f437b4db59e48f4a5
MarVikt <v.m.podporogie@gmail.com>
2022-05-12 17:30:08 +0300

    changed main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";console.log("первый модуль");console.log("второй модуль");})();
+(()=>{"use strict";(()=>{const e=document.querySelector(".top-menu"),t=document.querySelector(".header-wrapper");e.addEventListener("click",(e=>{if(e.target.matches('a[href*="#"]')){e.preventDefault();const o=e.target.getAttribute("href").substr(1);window.scrollTo({top:document.getElementById(o).offsetTop-(t?t.offsetHeight:100),behavior:"smooth"})}}))})(),console.log("второй модуль")})();

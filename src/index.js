fb555a8c8278ed81b9a110bd664cabff10abddb7
MarVikt <v.m.podporogie@gmail.com>
2022-05-14 21:00:52 +0300

    buttonUp is done

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import headerAnchors from "./modules/headerAnchors";
 import ourServices from "./modules/ourServices";
 import buttonServices from "./modules/buttonServices";
 import topSlider from "./modules/topSlider";
+import buttonUp from "./modules/buttonUp";
 
 
 headerAnchors();
 ourServices();
 buttonServices();
 topSlider();
+buttonUp();
---
+++ src/modules/buttonUp.js
@@ -0,0 +1,61 @@
+import{animate} from './helpers';
+
+const buttonUp = () => {
+  // @media only screen and (min-width: 992px)
+  if (window.screen.availWidth > 991) {
+    const upBtn = document.querySelector('img.up');
+    let upBtnIsDisplayed = false;
+  
+    upBtn.style.opacity = 0;
+    upBtn.style.cursor = 'default';
+  
+    window.addEventListener('scroll', () => {
+      if (document.documentElement.scrollTop > 600) {
+        if (!upBtnIsDisplayed) {
+          upBtn.style.cursor = 'pointer';
+          animate({
+            duration: 500,
+            timing(timeFraction) {
+              return Math.pow(timeFraction, 2);
+            },
+            draw(progress) {
+              upBtn.style.opacity = progress;
+            }
+          });
+          upBtnIsDisplayed = true;
+        }
+      } else {
+        if (upBtnIsDisplayed) {
+          animate({
+            duration: 500,
+            timing(timeFraction) {
+              return Math.pow(timeFraction, 2);
+            },
+            draw(progress) {
+              upBtn.style.opacity = 1 - progress;
+            }
+          });
+          upBtn.style.cursor = 'default';
+          upBtnIsDisplayed = false;
+        }
+      }
+    });
+  
+    upBtn.addEventListener('click', () => {
+      if (upBtnIsDisplayed) {
+        const currentScroll = document.documentElement.scrollTop;
+        animate({
+          duration: 1000,
+          timing(timeFraction) {
+            return timeFraction;
+          },
+          draw(progress) {
+            document.documentElement.scrollTop = (1 - progress) * currentScroll;
+          }
+        });
+      }
+    });
+  }
+};
+
+export default buttonUp;

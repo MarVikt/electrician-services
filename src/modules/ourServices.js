a6e251134593d908d8dea2a157b94fc671b41718
MarVikt <v.m.podporogie@gmail.com>
2022-05-17 14:30:26 +0300

    changed ourServices

--- src/modules/ourServices.js
+++ src/modules/ourServices.js
@@ -1,31 +1,50 @@
 const ourServices = () => {
   const servicesBlock = document.querySelector('.services-carousel');
   const items = document.querySelectorAll('.col-md-4');
-  const arrowLeft = document.querySelector('.services-arrow .arrow-left');
-  const arrowRight = document.querySelector('.services-arrow .arrow-right');
+  const arrowBlock = document.querySelector('.services-arrow');
+  const topPositionBlock = servicesBlock.offsetTop;
+  const leftPositionBlock = servicesBlock.offsetLeft;
   let count = 3;
   let itemWidht = 300; // ширина элемента
+  let widthBlock = servicesBlock.offsetWidth;
+  let prevItemLeftPosition = leftPositionBlock;
   let position = 0; // положение ленты прокрутки
 
-  items.forEach((item) => {
-    item.style.display = "inline";
-    itemWidht = item.clientWidth;
+  // console.dir(servicesBlock);
+  
+  items.forEach((item,index) => {
+    itemWidht = item.offsetWidth;
+    if (index === 0) {
+      servicesBlock.style.height = `${item.offsetHeight}px`;
+    }
+    if (item.offsetTop > topPositionBlock) {
+      item.style.top = `${topPositionBlock - item.offsetTop}px`;
+      item.style.left = `${prevItemLeftPosition + itemWidht - leftPositionBlock}px`;
+    }
+    prevItemLeftPosition = item.offsetLeft;
+  //     console.dir(item);
+  // console.log(prevItemLeftPosition);
   });
-  servicesBlock.style.widht = "2000px";
-  servicesBlock.style.display = "inline-block";
+  arrowBlock.style.width = `${widthBlock}px`;
 
-  arrowLeft.addEventListener('click', () => {
-    position += itemWidht * count;
-    // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
-    position = Math.min(position, 0);
-    servicesBlock.style.marginLeft = position + 'px';
-  });
-
-  arrowRight.addEventListener('click', () => {
-    position -= itemWidht * count;
-    // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
-    position = Math.max(position, -itemWidht * (items.length - count));
-    servicesBlock.style.marginLeft = position + 'px';
+  arrowBlock.addEventListener('click', (e) => {
+    if (e.target.matches('.arrow-left')) {
+      position += itemWidht * count;
+      // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+      position = Math.min(position, 0);
+      items.forEach(item => {
+        item.style.left = `${position}px`;
+      });
+      // servicesBlock.style.left = position + 'px';
+    } else if (e.target.matches('.arrow-right')) {
+      position -= itemWidht * count;
+      // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+      position = Math.max(position, -itemWidht * (items.length - count));
+      items.forEach(item => {
+        item.style.left = position + 'px';
+      });
+      // servicesBlock.style.left = position + 'px';
+    }
   });
 };
 

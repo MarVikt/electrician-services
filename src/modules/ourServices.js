e314ac91c62e180d84450fa5e5c2da8d55fb577d
MarVikt <v.m.podporogie@gmail.com>
2022-05-17 19:36:15 +0300

    slider is working

--- src/modules/ourServices.js
+++ src/modules/ourServices.js
@@ -1,7 +1,35 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+
 const ourServices = () => {
   const servicesBlock = document.querySelector('.services-carousel');
   const items = document.querySelectorAll('.col-md-4');
   const arrowBlock = document.querySelector('.services-arrow');
+
+  const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination],
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.arrow-right',
+      prevEl: '.arrow-left',
+    },
+
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+  
+  });
+
+  // document.body.addEventListener('click', (e) => {
+  //   console.log(e.target);
+  // });
 };
 
 export default ourServices;
